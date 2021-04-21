6a9475e3aa6d0a1879fef5639c425480a9cadb69
Visual9999 <68449061+Visual9999@users.noreply.github.com>
2021-04-21 10:48:40 +0200

    fixed the preload script. login page is default

--- electron/preload.js
+++ electron/preload.js
@@ -1,2 +1,13 @@
 const customTitlebar = require('custom-electron-titlebar');
-new customTitlebar.Titlebar({backgroundColor: customTitlebar.Color.fromHex('#2f3241')});
+const { contextBridge } = require('electron');
+
+function TitleBar(){
+	new customTitlebar.Titlebar({
+	backgroundColor: customTitlebar.Color.fromHex('#202225'),
+	itemBackgroundColor: customTitlebar.Color.fromHex('#282B2E'),
+	});
+}
+
+contextBridge.exposeInMainWorld('electron', {
+	buildTitleBar: TitleBar,
+}); 
