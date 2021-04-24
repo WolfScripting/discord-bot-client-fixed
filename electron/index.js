bd81072b2b1f9660d34b7320542c0b16bad60a71
Visual9999 <68449061+Visual9999@users.noreply.github.com>
2021-04-24 10:25:11 +0200

    added old index.js back i will fix soon

--- electron/index.js
+++ electron/index.js
@@ -1,37 +1,38 @@
 const { app, BrowserWindow, systemPreferences } = require("electron");
 const fetch = require("node-fetch");
 const btoa = require("btoa");
-const path = require('path');
+// const fs = require("fs");
 
 async function createWindow() {
 	var html = await fetch("https://raw.githubusercontent.com/Flam3rboy/discord-bot-client/master/index.html");
+	// html = fs.readFileSync(__dirname + "/../index.html");
 	html = await html.text();
 	// Create the browser window.
 	let win = new BrowserWindow({
 		width: 1920,
 		height: 1080,
-		icon: path.join(__dirname, 'buildResources', 'icon.png'),
-		preload: 'preload.js',
-		titleBarStyle: "hidden",
+		icon: __dirname + "/buildResources/icon.png",
 		webPreferences: {
 			webSecurity: true,
 			nodeIntegration: false,
-			enableRemoteModule: true,
+			enableRemoteModule: false,
 			contextIsolation: true,
 		},
 	});
+	// win.webContents.openDevTools();
 	win.webContents.on("did-navigate", () => {
 		win.webContents.executeJavaScript(`document.write(atob("${btoa(html)}"))`);
-		win.webContents.executeJavaScript(`window.electron.buildTitleBar()`);
 	});
-	
 	if (systemPreferences && systemPreferences.askForMediaAccess) systemPreferences.askForMediaAccess("microphone");
 	win.webContents.on("new-window", function (e, url) {
 		e.preventDefault();
 		require("electron").shell.openExternal(url);
 	});
 	win.loadURL("https://blank.org");
-	
+	// win.loadURL("data:text/html;charset=UTF-8," + encodeURIComponent(html), {
+	// 	baseURLForDataURL: `file://${__dirname}/app`,
+	// });
+
 	const filter = {
 		urls: ["<all_urls>"],
 	};
